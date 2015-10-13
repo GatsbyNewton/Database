11629a6f650d86fb8acbc360bf741d01082b4e53
wangzmking@163.com <wangzmking@163.com>
2015-10-13 20:14:58 +0800

    sql file

---
+++ src/main/resources/user.sql
@@ -0,0 +1,34 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : test
+Source Server Version : 50540
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50540
+File Encoding         : 65001
+
+Date: 2015-10-13 09:54:04
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) NOT NULL,
+  `age` int(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'Bill', '34');
+INSERT INTO `user` VALUES ('2', 'Tom', '20');
+INSERT INTO `user` VALUES ('3', 'Jim', '50');
